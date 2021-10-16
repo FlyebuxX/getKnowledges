05c6cbe15259ea9b5b8a3acd98cfa6f79cae61d4
FlyebuX <valentin.eberhardt57@gmail.com>
2021-10-16 11:45:00 +0200

    16/10 : Update Generator | Creating Loadings

--- generator.py
+++ generator.py
@@ -3,8 +3,8 @@
 # ======================================================================================================================
 
 
-from paths import Path
-import random
+import paths
+import loadings
 # ======================================================================================================================
 # CLASS
 # ======================================================================================================================
@@ -20,14 +20,29 @@ class Generator:
         self.gamemode = gamemode
         self.path = language + "Package"
 
-    def paths(self):
-        """
-        Paths function
-        """
-        new_path = Path()
+        new_path = paths.Path(self.path)
 
         self.words_list = new_path.words_list()
-        self.in_progress_list = new_path.in_progresss_list()
+        self.in_progress_list = new_path.in_progress_list()
         self.acquired_words = new_path.acquired_words()
         self.foreign_words = new_path.foreign_words(self.lang)
         self.french_words = new_path.french_words()
+
+    def files(self):
+        """
+        """
+        self.words_list = loadings.file_loading(self.words_list)
+        self.in_progress_list = loadings.file_loading(self.in_progress_list)
+        self.acquired_words = loadings.file_loading(self.acquired_words)
+        self.foreign_words = loadings.file_loading(self.foreign_words)
+        self.french_words = loadings.file_loading(self.french_words)
+
+
+# ======================================================================================================================
+# MAIN
+# ======================================================================================================================
+
+
+gen = Generator("english", "english")
+gen.files()
+
--- paths.py
+++ paths.py
@@ -3,7 +3,6 @@
 # ======================================================================================================================
 
 
-from generator import Generator
 # ======================================================================================================================
 # CLASS
 # ======================================================================================================================
@@ -14,26 +13,26 @@ class Path:
     Class which defines english propreties
     """
 
-    def __init__(self):
-        self.path = Generator.path
+    def __init__(self, path):
+        self.path = path
 
     def words_list(self):
         """
         Creating the wordsList
         :return: words_list, list
         """
-        LIST = open(self.path + "/words.txt")
+        LIST = open(self.path + "/words.txt", "r", encoding="utf-8")
         words_list = LIST.readlines()
         LIST.close()
 
         return words_list
 
-    def in_progess_list(self):
+    def in_progress_list(self):
         """
         Creating the inProgressList
         :return: in_progress_list, list
         """
-        LIST = open(self.path + "/inProgressWords.txt")
+        LIST = open(self.path + "/inProgressWords.txt", "r", encoding="utf-8")
         in_progress_list = LIST.readlines()
         LIST.close()
 
@@ -44,7 +43,7 @@ class Path:
         Creating the acquiredWordsList
         :return: acquired_words, list
         """
-        LIST = open(self.path + "/acquiredWords.txt")
+        LIST = open(self.path + "/acquiredWords.txt", "r", encoding="utf-8")
         acquired_words = LIST.readlines()
         LIST.close()
 
@@ -55,17 +54,21 @@ class Path:
         Creating the foreignWords list
         :return: foreign_words, list
         """
-        LIST = open(self.path + "/" + lang + "Words.txt")
+        LIST = open(self.path + "/" + lang + "Words.txt", "r", encoding="utf-8")
         foreign_words = LIST.readlines()
         LIST.close()
 
         return foreign_words
 
-    def french_words(self, lang):
+    def french_words(self):
         """
         Creating the frenchWords list
         :param lang: str, language
         :return: french_words, list
         """
-        LIST = open(self.path + "/frenchWords.txt")
+        LIST = open(self.path + "/frenchWords.txt", "r", encoding="utf-8")
+        french_words = LIST.readlines()
+        LIST.close()
+
+        return french_words
 
