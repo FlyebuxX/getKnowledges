9ca4f055674c5fbfd88f5347901f344c10c14168
FlyebuX <valentin.eberhardt57@gmail.com>
2021-10-23 17:51:47 +0200

    Typo Fixed + Doc

--- methods.py
+++ methods.py
@@ -1,38 +1,31 @@
 # ======================================================================================================================
-# IMPORTS
-# ======================================================================================================================
-
-
-from generator import Generator
-from paths import Path
-# ======================================================================================================================
 # CLASS
 # ======================================================================================================================
 
 
 class Method:
     """
-    Methods which can useful to deal with files
+    Methods which manage with files
     """
 
     def __init__(self, file_name, path):
         self.file = file_name
         self.path = path
 
-    def add_in_file(self, toWrite):
+    def add_in_file(self, to_write):
         """
         Adding in a file
         :return:
         """
         file = open(self.path + "/" + self.file, "a", encoding="utf-8")
-        file.write(toWrite)
+        file.write(to_write)
         file.close()
 
-    def write_in_file(self, toWrite):
+    def write_in_file(self, to_write):
         """
         Writing in a file
         :return:
         """
         file = open(self.path + "/" + self.file + "\n", "r", encoding="utf-8")
-        file.write(toWrite)
+        file.write(to_write)
         file.close()
